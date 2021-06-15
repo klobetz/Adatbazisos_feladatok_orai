d6cb9402f9b0191edc7afe226cce97ef6a4a2015
Ákos Klobetz <klobetz@gmail.com>
2021-06-15 18:34:45 +0200

    #4: exportálás tokkal vonóval

--- DBF_WPF_projekt_01/KeresesWindow.xaml.cs
+++ DBF_WPF_projekt_01/KeresesWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ClosedXML.Excel;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -50,7 +53,68 @@ namespace DBF_WPF_projekt_01
 
         private void btn_export_Click(object sender, RoutedEventArgs e)
         {
+            //Az excel létrehozása
+            using (var workbook = new XLWorkbook())
+            {
+                var ws = workbook.Worksheets.Add("kolcsonzes"); //a munkalap elnevezése
 
+                //kell az oszlopok és a sorok továbbléptetéséhez
+                int column = 1;
+                int row = 1;
+
+                try
+                {
+                    //a fejléc elkészítése
+                    foreach (var fejlec in dg_keres.Columns)
+                    {
+                        ws.Cell(row, column).Value = fejlec.Header; //fejléc elkészítés
+
+                        ws.Cell(row, column).Style.Font.Bold = true; //a szöveg félkövér
+
+                        column++;
+                    }
+
+                    column = 1;
+                    row = 2; //mert különben felülírja a fejlécet!
+
+                    //adatok feltöltése a sorokba
+                    foreach (Kolcsonzo item in dg_keres.Items)
+                    {                       
+                        ws.Cell(row, column++).Value = item.Nev;
+                        ws.Cell(row, column++).Value = item.Cim;
+                        ws.Cell(row, column++).Value = item.Auto == null ? null : item.Auto.Marka; //az üres mezökre hiba javítás
+                        ws.Cell(row, column++).Value = item.Auto == null ? null : item.Auto.Tipus;
+                        ws.Cell(row, column).Value = item.Berlo == null ? null : item.Berlo.Nev;
+                                                
+                        row++;
+                        column = 1; //vissza kell állítani mert különben elcsúszik a sor kiíratása
+                    }
+
+                    ws.Columns().AdjustToContents(); //oszlopszéleség automatkus beállítása
+
+                    var fajlmentes = new SaveFileDialog() {Filter = "Excel fájlok|*.xlsx"}; //filtre ==> xlsx kiterjesztést ne keljen megadni
+                    
+                    if (fajlmentes.ShowDialog() == true)
+                    {
+                        workbook.SaveAs(fajlmentes.FileName); //én nevezem el az állományomat
+                        MessageBox.Show("Elkészült a mentés"); //figyelmeztet
+                    }
+                    else
+                    {
+                        //ha bezárom a dialógus ablakot
+                        MessageBox.Show("Az adatokat nem mentette el!");
+                    }
+                    
+
+                    
+                }
+                catch (Exception ex) //ex==> kivétel hibakezelés megmutatására 
+                {
+                    //MessageBox.Show(ex.ToString()); //hiba kiíratása
+                    MessageBox.Show("Hiba az adatok metése közben!","Figyelmeztetés",MessageBoxButton.OK,MessageBoxImage.Error);
+                }
+
+            }
         }        
     }
 }
